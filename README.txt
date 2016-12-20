28f900115a16b05694b1e3ef492f73d2d2aebc79
chocolateashes <ashwini.velchamy@gmail.com>
2016-12-19 20:38:24 -0500

    added features

---
+++ README.txt
@@ -0,0 +1,20 @@
+Nermai Web Application
+
+-Must have Python3
+-Pip must be installed
+	To install on Windows, download and open get-pip.py
+	To install on Mac OS X & Linux, run
+		$ curl -O https://bootstrap.pypa.io/get-pip.py
+		$ python get-pip.py --user
+-Install Flask
+	$ pip install flask
+
+————————
+
+To Run Nermai
+-Download zip file Nermai.zip
+-Navigate to Nermai directory in terminal or command prompt
+-Run python hello.py (if that doesn’t work try python3 hello.py)
+-At this point the terminal should say something like
+	*Running on http://127.0.0.1:5000/
+-Open that address in your browser (localhost:5000)
--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
+#Hello.py is the python file that has the functions that render templates and push out SQL queries
 from __future__ import print_function # In python 2.7
 import sys 
 from flask import Flask, render_template
 from flask import request, redirect
 from flask import g
 import sqlite3
+
 app = Flask(__name__)
 
-officials = {}
-
+#connecting to the database which includes both opening it in before_request and closing it in teardown_request
 @app.before_request
 def before_request():
     g.db = sqlite3.connect("tnofficials.db")
@@ -17,19 +18,24 @@ def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
 
+#begins the application by rendering the index.html template
 @app.route("/")
 def begin():
     return render_template("index.html")
 
+#called when "reportofficials" is called in the HTML
 @app.route('/reportofficials')
 def reportofficials():
-    """ Displays the index page accessible at '/'
-    """
     return render_template('reportofficials.html')
+
+#called when "about" is called in the HTML
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+#called when "report" is called in the HTML
+#this is the result of submitting a form which is why the POST method is used
+#pulls data drom the form and then uses an Insert statement to push into the database
 @app.route('/report', methods = ['POST'])
 def report():
     name = request.form['name']
@@ -39,40 +45,40 @@ def report():
     date = request.form['date']
     message = request.form['message']
 
+   
     g.db.execute("INSERT INTO tamilofficials(name, department, county, star, date, message) VALUES (?,?,?,?,?,?)", [name,department,county,star,date,message])
     g.db.commit()
 
-    
-    #if name in officials:
-    #    officials[name] = officials[name] + int(star)
-    #else:
-    #    officials[name] = int(star)
-
-    #print("Name: " + name)
-    #print("Department: " + department)
-    #print("County: " + county)
-    #print("Date: " + date)
-    #print("Rating: " + star)
-
     return redirect('/')
 
+#called when "officials" is called in the HTML
+#populates the HTML with data from the database
+#performs a SELECT query in order to retrieve all the names from the database
 @app.route('/officials.html')
 def officials():
     tamilofficials = g.db.execute("SELECT name from tamilofficials").fetchall()
     return render_template('officials.html', tamilofficials = tamilofficials)
 
+#called when "toprated" is called in the HTML
+#populates the HTML with data from the database
+#performs a SELECT query in order to retrieve all the names from the database where the star rating is greater than 3
+@app.route('/toprated.html')
+def toprated():
+    topofficials = g.db.execute("SELECT name from tamilofficials where star > 3").fetchall()
+    return render_template('toprated.html', topofficials = topofficials)
+
+#called when "details" is called in the HTML
+#has a card pop up with the details from the database
+#perfotms multiple select queries and outputs to the HTML
+#takes in the name as an input in order to use in the queries
 @app.route('/details/<name>')
 def details(name):
-    #name = str(name[0])
- 
-    print(name, file=sys.stderr)
     officialname = g.db.execute("SELECT name from tamilofficials where name = ?", (name,)).fetchall()
-    #return render_template('details.html', officialname = officialname)
     officialdepartment = g.db.execute("SELECT department from tamilofficials where name = ?", (name,)).fetchall()
     officialcounty = g.db.execute("SELECT county from tamilofficials where name = ?", (name,)).fetchall()
     officialrating = g.db.execute("SELECT star from tamilofficials where name = ?", (name,)).fetchall()
     officialmessage = g.db.execute("SELECT message from tamilofficials where name = ?", (name,)).fetchall()
-#print(officialname)
+    
     return render_template('details.html', officialname = officialname, officialdepartment = officialdepartment, officialcounty=officialcounty, officialrating=officialrating, officialmessage = officialmessage)
  
 
